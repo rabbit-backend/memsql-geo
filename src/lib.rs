c4e07cf6b3ad454d50fe5f4cd3367a24400a523a
rohit20001221 <rohit20001221@gmail.com>
2025-11-14 02:00:22 +0530

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -64,9 +64,10 @@ fn _st_tile_envelope(z: u8, x: u64, y: u64) -> String {
     match tms().lookup("WebMercatorQuad") {
         Ok(tms) => {
             let bounds = tms.xy_bounds(&Xyz::new(x, y, z));
-            let (x1, y1, x2, y2) = (bounds.left, bounds.bottom, bounds.right, bounds.top);
+            let (min_x, min_y, max_x, max_y) =
+                (bounds.left, bounds.bottom, bounds.right, bounds.top);
 
-            Rect::new(coord! {x: x1, y: y1}, coord! {x: x2, y: y2})
+            Rect::new(coord! {x: min_x, y: min_y}, coord! {x: max_x, y: max_y})
                 .to_polygon()
                 .to_wkt()
                 .to_string()
